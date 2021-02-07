4b239337d3409e9337e05b56d4e8eb2bfce859e6
ThaRising <kochbe.ber@gmail.com>
2021-02-07 12:24:05 +0100

    Disable SSL requirements for database

--- BauerDude/settings/production.py
+++ BauerDude/settings/production.py
@@ -61,9 +61,7 @@ MIDDLEWARE = [
 
 """ ORM Config """
 DATABASES = {
-    'default': dj_database_url.config(
-        conn_max_age=600, ssl_require=True
-    )
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 
